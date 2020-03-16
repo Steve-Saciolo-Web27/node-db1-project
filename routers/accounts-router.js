ae74dd26f94cf373d050b363a1610f2925e54b6e
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-03-16 15:30:45 -0400

    Configured get and get by id requests

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -4,6 +4,7 @@ const db = require('../data/dbConfig')
 
 const router = express.Router()
 
+// READ/GET
 router.get('/', (req, res) => {
   db.select('*')
     .from('accounts')
@@ -12,6 +13,19 @@ router.get('/', (req, res) => {
     })
     .catch(error => {
       res.status(500).json({ error: 'Internal Server Error' })
+    })
+})
+
+// READ/GET By ID
+router.get('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      res.status(200).json(account)
+    })
+    .catch(error => {
+      res.status(404).json({ error: 'Account with specified ID not found!' })
     })
 })
 
